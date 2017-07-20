faf1db5191f067f8bcd856d3f0774b464f690cad
Zhu Na <nanazhu@users.noreply.github.com>
2017-07-20 20:03:12 +0200

    retrieve data from current working API

--- RetriveData/src/main/java/RetrieveData.java
+++ RetriveData/src/main/java/RetrieveData.java
@@ -7,6 +7,7 @@ import java.time.ZonedDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -19,15 +20,12 @@ import static org.hamcrest.Matchers.greaterThan;
 public class RetrieveData {
 
     private final  String accessTokenUrl = "https://sso.sparkworks.net/aa/oauth/token";
-
-    private final  String clientId = "spark";
-
-    private final  String clientSecret = "spark";
-
     private final  String grantType ="password";
 
-    private final  String username = "ro_schools";
+    private final  String clientId = "spark";
+    private final  String clientSecret = "spark";
 
+    private final  String username = "ro_schools";
     private final  String password="Readonly1234";
 
     private static String accessToken;
@@ -44,7 +42,6 @@ public class RetrieveData {
 
     private void authenticate() throws Exception {
         useRelaxedHTTPSValidation();
-        // todo internet check
         JsonPath.config = new JsonPathConfig("UTF-8");
         String response = given().accept(ContentType.JSON)
                 .params("username", username, "password", password, "grant_type", grantType, "scope", "read")
@@ -55,40 +52,8 @@ public class RetrieveData {
         System.out.println("Access_token: "+ accessToken);
     }
 
-    private Long getResourceIdRandom(List<Long> resourceIds) throws Exception, AssertionError {
-        Long resourceId = resourceIds.get(new Random().nextInt(resourceIds.size()));
-        System.out.println("CHECKOUT the resouceId randomly " + resourceId);
-
-        String get_str = "/resource/" + Long.toString(resourceId);
-        String response = given().spec(requestSpecBuilder.build())
-                .auth().preemptive().oauth2(accessToken)
-                .get(get_str)
-                .then().assertThat()
-                .statusCode(200)
-                .body("resourceId", equalTo(resourceId.intValue()))
-                .extract()
-                .body().asString();
-        System.out.println(from(response).get("property"));
-        return resourceId;
-    }
-
-    private Long getSiteIdRandom(List<Long> siteIds) throws Exception, AssertionError {
-        Long siteId = siteIds.get(new Random().nextInt(siteIds.size()));
-        System.out.println("CHECKOUT the siteId randomly " + siteId);
-
-        String get_str = "/location/site/" + Long.toString(siteId);
-        given().spec(requestSpecBuilder.build())
-                .auth().preemptive().oauth2(accessToken)
-                .get(get_str)
-                .then().assertThat()
-                .statusCode(200)
-                .body("id", equalTo(siteId.intValue()))
-                .extract()
-                .body().asString();
-        return siteId;
-    }
-
     private List<Long> listSitesIds() throws Exception, AssertionError {
+//        GET /v1/location/site Retrieve a collection of Sites
         String get_str = "/location/site";
         String response = given()
                 .spec(requestSpecBuilder.build())
@@ -101,11 +66,12 @@ public class RetrieveData {
                 .body().asString();
         List<Long> siteIds = from(response).getList("sites.id", Long.class);
         System.out.println("CHECKOUT the siteIds list " + siteIds);
-        
+
         return siteIds;
     }
 
     private List<Long> listResourcesIds(Long siteId) throws Exception, AssertionError {
+//        GET /v1/location/site/{siteId}/resource Retrieve the Resources of a Site
         String get_str = "/location/site/" + Long.toString(siteId) + "/resource";
         String response = given()
                 .spec(requestSpecBuilder.build())
@@ -121,7 +87,29 @@ public class RetrieveData {
         return resourceIds;
     }
 
-    private void getResourceIdProperty(Long resourceId , String property) throws Exception, AssertionError {
+    private Long getSiteIdRandom(List<Long> siteIds) throws Exception, AssertionError {
+//        GET /v1/location/site/{siteId} Retrieve Site by the site unique identifier
+        Long siteId = siteIds.get(new Random().nextInt(siteIds.size()));
+        System.out.println("CHECKOUT the siteId randomly " + siteId + " and also some details");
+
+        String get_str = "/location/site/" + Long.toString(siteId);
+        String response = given().spec(requestSpecBuilder.build())
+                .auth().preemptive().oauth2(accessToken)
+                .get(get_str)
+                .then().assertThat()
+                .statusCode(200)
+                .body("id", equalTo(siteId.intValue()))
+                .extract()
+                .body().asString();
+        System.out.println(response);
+//        System.out.println(from(response).getList("subsites.user.username"));
+        return siteId;
+    }
+
+    private Long getResourceIdRandom(List<Long> resourceIds) throws Exception, AssertionError {
+//        GET /v1/resource/{resourceId} Retrieve a Spark Works Resource Details by its unique identifier
+        Long resourceId = resourceIds.get(new Random().nextInt(resourceIds.size()));
+        System.out.println("CHECKOUT the resouceId randomly " + resourceId + " and check its properity");
         String get_str = "/resource/" + Long.toString(resourceId);
         String response = given().spec(requestSpecBuilder.build())
                 .auth().preemptive().oauth2(accessToken)
@@ -131,39 +119,96 @@ public class RetrieveData {
                 .body("resourceId", equalTo(resourceId.intValue()))
                 .extract()
                 .body().asString();
-        System.out.println("resouceId "+ resourceId + " : " + from(response).get(property));
+        System.out.println(from(response).get("property"));
+        return resourceId;
     }
 
-    private void getResourceLatestData(Long resourceId) throws Exception {
-        String get_str = "/resource/" + Long.toString(resourceId) + "/latest";
-        String response = given()
-                .spec(requestSpecBuilder.build())
-                .auth().preemptive().oauth2(accessToken)
-                .get(get_str)
-                .then().assertThat()
-                .statusCode(200)
-                .body("id", equalTo(resourceId.intValue()))
-                .extract()
-                .body().asString();
-        String latestValue = from(response).get("latest").toString();
-        System.out.println("Resource "+ resourceId + " latest value: " + latestValue);
-    }
-    
-    private void getResourceSummary(Long resourceId) throws Exception {
-        String get_str = "/resource/"+ Long.toString(resourceId)+"/summary";
-        String response = given()
-                .spec(requestSpecBuilder.build())
+    private void getresouceIDTag(Long resourceId) throws Exception, AssertionError {
+//      GET /v1/resource/{resourceId}/tag Retrieve the tags of a Resource by the resource unique identifier
+//      TODO  no tag in the raw data yet
+        String get_str = "/resource/"+ Long.toString(resourceId)+"/tag";
+        String response = given().spec(requestSpecBuilder.build())
                 .auth().preemptive().oauth2(accessToken)
                 .get(get_str)
                 .then().assertThat()
                 .statusCode(200)
                 .extract()
                 .body().asString();
-        List<Long> data = from(response).getList("minutes60");
-        System.out.println("Resource "+ resourceId+ " in  \"minutes60\": "+ data);
+        List<Long> data = from(response).getList("tag");
+        System.out.println(data);
     }
 
-    private void getResourceDataByDayRange(Long resourceId, ZonedDateTime start, ZonedDateTime end, String frequency) {
+    private void getResourceIdDetails(Long resourceId , String detail) throws Exception, AssertionError {
+//        GET /v1/resource/{resourceId} Retrieve a Spark Works Resource Details by its unique identifier
+//       TODO GET /v1/resource/{resourceId}/property Retrieve the properties of a Resource by the resource unique identifier
+//       TODO since all the information we need could be retrieved from the first API i dont get why we need the second one
+        try{
+            System.out.println("CHECKOUT the resouceId " + resourceId + " with details "+ detail);
+            String get_str = "/resource/" + Long.toString(resourceId);
+            String response = given().spec(requestSpecBuilder.build())
+                    .auth().preemptive().oauth2(accessToken)
+                    .get(get_str)
+                    .then().assertThat()
+                    .statusCode(200)
+                    .body("resourceId", equalTo(resourceId.intValue()))
+                    .extract()
+                    .body().asString();
+            System.out.println("resouceId "+ resourceId + " : " + from(response).get(detail));
+        }
+        catch(AssertionError e){
+            System.out.println("getResourceIdDetails " + resourceId + " " + e);
+        }
+    }
+
+    private void getResourceLatest(Long resourceId,String latest_item) throws Exception,AssertionError {
+//        GET /v1/resource/{resourceId}/latest Retrieve latest values of a Resource
+        try {
+            String get_str = "/resource/" + Long.toString(resourceId) + "/latest";
+            String response = given()
+                    .spec(requestSpecBuilder.build())
+                    .auth().preemptive().oauth2(accessToken)
+                    .get(get_str)
+                    .then().assertThat()
+                    .statusCode(200)
+                    .body("id", equalTo(resourceId.intValue()))
+                    .extract()
+                    .body().asString();
+            String latestValue = from(response).get(latest_item).toString();
+            System.out.println("Resource " + resourceId + " latest value: " + latestValue);
+        }
+        catch(AssertionError e){
+            System.out.println("getResourceLatest " + resourceId + " " + e);
+        }
+    }
+
+    private void getResourceSummary(Long resourceId,String frequency) throws Exception,AssertionError {
+//        GET /v1/resource/{resourceId}/summary Retrieve latest summary values of a Resource
+//        GET /v1/resource/{resourceId}/summary/{targetUom} Retrieve latest summary values of a Resource
+//        TODO same out for adding Uom with ID
+        try {
+            String get_str = "/resource/" + Long.toString(resourceId) + "/summary";
+            String response = given()
+                    .spec(requestSpecBuilder.build())
+                    .auth().preemptive().oauth2(accessToken)
+                    .get(get_str)
+                    .then().assertThat()
+                    .statusCode(200)
+                    .extract()
+                    .body().asString();
+            if ("min" == frequency || "max" == frequency || "average" == frequency) {
+                Map<String, Integer> data = from(response).getMap(frequency);
+                System.out.println("Resource " + resourceId + " in  \"" + frequency + "\": " + data);
+            } else {
+                List<Long> data = from(response).getList(frequency);
+                System.out.println("Resource " + resourceId + " in  \"" + frequency + "\": " + data);
+            }
+        }
+        catch(AssertionError e){
+                System.out.println("getResourceSummary " + resourceId + " " + e);
+            }
+    }
+
+    private void getResourceDataByDayRange(Long resourceId, ZonedDateTime start, ZonedDateTime end, String frequency) throws Exception,AssertionError {
         System.out.println("Resource  historical data resource ID: "+ resourceId +" from : " +  start.toInstant()+" to "
                 +  end.toInstant() + " with steps per " + frequency );
         String requestBody = "" +
@@ -207,7 +252,7 @@ public class RetrieveData {
 //            System.out.println(Arrays.toString(groups));
         }
         catch (AssertionError e){
-            System.out.println("Nothing available on "+ resourceId);
+            System.out.println("getResourceDataByDayRange " +resourceId + " " + e);
 
         }
     }
@@ -219,29 +264,21 @@ public class RetrieveData {
 
         ZonedDateTime start = ZonedDateTime.now().minusWeeks(1);
         ZonedDateTime end = ZonedDateTime.now();
-
         List<Long> siteIds = instance.listSitesIds();
+        Long siteIdXX = instance.getSiteIdRandom(siteIds);
+        List<Long> resourceIdsXX = instance.listResourcesIds(siteIdXX);
+        Long resourceIdXX = instance.getResourceIdRandom(resourceIdsXX);
 
         // looping  trial
         System.out.println("Check out the resources data");
         for (Long siteId : siteIds){
             List<Long> resourcesIds = instance.listResourcesIds(siteId);
             for (Long resourceId: resourcesIds){
-                instance.getResourceLatestData(resourceId);
-                instance.getResourceSummary(resourceId);
-                instance.getResourceIdProperty(resourceId,"property");
+                instance.getResourceIdDetails(resourceId,"property");
+                instance.getResourceLatest(resourceId,"latestMonth");
+                instance.getResourceSummary(resourceId,"max");
                 instance.getResourceDataByDayRange(resourceId, start, end, "day");
             }
         }
-
-        // SINGLE trial
-//        Long siteId = instance.getSiteIdRandom(siteIds);
-//        List<Long> resourcesIds = instance.listResourcesIds(siteId);
-//        Long resourceId = instance.getResourceIdRandom(resourcesIds);
-//        System.out.println("Following is the data we monitored");
-//        instance.getResourceLatestData(resourceId);
-//        instance.getResourceSummary(resourceId);
-//        instance.getResourceIdProperty(resourceId,"property");
-//        instance.getResourceDataByDayRange(resourceId, start, end, "day");
     }
 }
