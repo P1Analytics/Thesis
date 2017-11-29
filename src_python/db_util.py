c953217c3503a55bd67b28afd271f6e2712ec02b
nanazhu <nana.zhu@hotmail.com>
2017-11-30 00:36:03 +0100

    New create index for table query pandas dataframe - import-able csv file
    Change
    spread resource_value into different site

--- src_python/db_util.py
+++ src_python/db_util.py
@@ -2,6 +2,7 @@ import sqlite3
 from sqlite3 import Error
 import pandas as pd
 import glob
+import os
 
 
 def create_connection(db_file):
@@ -22,6 +23,10 @@ def execute_sql(cursor, sql_command):
 
 
 def init_database(cursor):
+    """
+    :param cursor:
+    :return: an empty database with initialized tables
+    """
     sql_create_orientation_table = """ 
                                     CREATE TABLE IF NOT EXISTS  orientation (
                                         site  INT,
@@ -52,46 +57,67 @@ def init_database(cursor):
                                         );"""
     execute_sql(cursor, sql_create_details_site_table)
 
-    sql_create_resource_value_table = """ CREATE TABLE IF NOT EXISTS resource_value (
-                                        id INT,
-                                        time  DATETIME,
-                                        value  REAL
-                                    );"""
-    execute_sql(cursor, sql_create_resource_value_table)
 
-    sql_create_resource_no_outliers_table = """ CREATE TABLE IF NOT EXISTS resource_no_outliers  (
+    sql_create_API_Temperature_table = """
+                                            CREATE TABLE IF NOT EXISTS API_Temperature  (
                                             id INT,
                                             time  DATETIME,
                                             value  REAL
-                                        );"""
-    execute_sql(cursor, sql_create_resource_no_outliers_table)
+                                            );
+                                        """
 
-    sql_create_resource_ETL_table = """ CREATE TABLE IF NOT EXISTS resource_ETL  (
-                                            id INT,
-                                            time  DATETIME,
-                                            value  REAL
-                                        );"""
-    execute_sql(cursor, sql_create_resource_ETL_table)
+    execute_sql(cursor, sql_create_API_Temperature_table)
+
+    sql_create_API_CloudCoverage_table = """
+                                                CREATE TABLE IF NOT EXISTS API_CloudCoverage  (
+                                                id INT,
+                                                time  DATETIME,
+                                                value  REAL
+                                                );
+                                            """
+    execute_sql(cursor, sql_create_API_CloudCoverage_table)
+
+
+def pandas_to_sqlite(df):
+    """
+    :param df:
+    :return: sqlite execute command
+    """
+    directory = './DBimport/'
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    # df head : [ timestamps id1  id2 ...idn]
+    for value in list(df)[1::]:
+        print(value)
+        df_value = df[['timestamps', value]]
+        df_value["id"] = [value] * df.shape[0]
+        new = ["id", "timestamps", value]
+        df_value = df_value.reindex(columns=new)
+        print(df_value)
+        df_value.to_csv(directory + value + "to_import_sqlite.csv", sep=";", index=False, header=False)
+    importFromCSV(directory, "resource_value")
 
 
 def importFromCSV(folder, table):
-    file_list = glob.glob(folder + "*.csv")
     # print("Copy paste below dot command into your sqlite \n! Nothing execute in here\n")
+
+    file_list = glob.glob(folder + "*.csv")
     print(".separator \";\"")
     for i in file_list:
         print(".import " + i + " " + table)
-    print("""delete from resource_value where rowid not in 
-    (select  min(rowid) from resource_value group by id,time,value);""")
+    print(
+        "delete from " + table + " where rowid not in (select  min(rowid) from " + table + " group by id,time,value);")
 
 
 def exportToCSV(query, file):
+    # sqlite > select time, value from resource_value where time > '2017-03-21' and time < '2017-03-22';
     # print("Copy paste below dot command into your sqlite \n! Nothing execute in here\n")
     print(".headers on")
     print(".mode csv")
     print(".separator \";\"")
     print(".output " + file)
     print(query)
-    # sqlite > select time, value from resource_value where time > '2017-03-21' and time < '2017-03-22';
 
 
 def query_temp_resource_value(cursor, temp_resource_list):
@@ -102,10 +128,11 @@ def query_temp_resource_value(cursor, temp_resource_list):
     return resource_value
 
 
-def query_site_room_orientaion(cursor, room_list):
-    for id in room_list:
-        orient = [i for i in cursor.execute("select room, orien from orientation where room =" + str(id))]
-    return orient
+def query_site_room_orientaion(cursor, siteID):
+    # print("select room, orien from orientation where site =" + str(siteID) + ";")
+    orient = cursor.execute("select room, orien from orientation where site =" + str(siteID))
+    # print(orient.fetchall()) # tuple [id,'NE']
+    return orient.fetchall()
 
 
 def query_temperature_resource(cursor, site_id):
@@ -118,60 +145,86 @@ def query_temperature_resource(cursor, site_id):
 
 
 def select_data_to_col(cursor, query):
-    # resp = cursor.execute("select time, value from resource_value "
-    #                  "where id = " + str(id)
-    #                  + " and time > '2017-03-01' and time < '2017-04-01';")
     resp = cursor.execute(query)
     df = pd.DataFrame(resp.fetchall(), columns=["timestamps", id], dtype=float)
     df = df.reset_index(drop=True)
     df = df.set_index('timestamps')
     df = df[~df.index.duplicated(keep='first')]
+
     return df
 
 
-def select_time_range_to_dataframe(cursor, resource_list, begin, end):
+def select_time_range_to_dataframe(cursor, site_id, resource_list, begin, end):
     df_final = pd.DataFrame()
     for id in resource_list:
-        resp = cursor.execute("select time, value from resource_value "
-                              "where id = " + str(id)
+        resp = cursor.execute("select time, value from site_" + site_id +
+                              " where id = " + str(id)
                               + " and time > '" + begin + "' and time < '" + end + "';")
-        # + " and time > '2017-03-01' and time < '2017-04-01';")
         df = pd.DataFrame(resp.fetchall(), columns=["timestamps", id], dtype=float)
         df = df.reset_index(drop=True)
         df = df.set_index('timestamps')
         df = df[~df.index.duplicated(keep='first')]
         df_final = pd.concat([df_final, df], axis=1)
-        print(df_final.shape)
-    df_final.to_csv(df_final + ".csv")
+    # df_final.to_csv("df_final.csv")
     return df_final
 
 
+def create_resource_value_tables(cursor):
+    site_list = c.execute("select site from details_sensor group by site;")
+    site_list = [str(id[0]) for id in site_list]
+    for site_id in site_list:
+        sql_create_site_i_table = " CREATE TABLE IF NOT EXISTS site_" + site_id + " (id INT,time DATETIME ,value  REAL);"
+        # print(sql_create_site_i_table)
+        execute_sql(cursor, sql_create_site_i_table)
+
+
+def create_resource_ETL_tables(cursor):
+    site_list = c.execute("select site from details_sensor group by site;")
+    site_list = [str(id[0]) for id in site_list]
+    for site_id in site_list:
+        sql_create_site_i_table = " CREATE TABLE IF NOT EXISTS site_" + site_id + "_ETL (id INT,time DATETIME ,value  REAL);"
+        # print(sql_create_site_i_table)
+        execute_sql(cursor, sql_create_site_i_table)
+
+
+def create_index_resource_value_tables(cursor, site_i_table):
+    """
+    speed up the performance by creating index on the table on : id(aka resource id) and  id+time
+    :param cursor:
+    :param site_i_table: for site i contains [ id , time,value ]
+    :return: None
+    Ref: https://medium.com/@JasonWyatt/squeezing-performance-from-sqlite-indexes-indexes-c4e175f3c346
+    """
+    print("CREATE INDEX id ON " + site_i_table + " (id);")
+    print("CREATE INDEX id_time ON " + site_i_table + " (id,time);")
+    cursor.execute("CREATE INDEX id ON " + site_i_table + " (id);")
+    cursor.execute("CREATE INDEX id_time ON " + site_i_table + " (id,time);")
+
+
 if __name__ == "__main__":
-    database = 'test.db'
-
-    with create_connection(database) as conn:
+    with create_connection('/Users/nanazhu/Documents/Sapienza/Thesis/src_python/test.db') as conn:
         try:
-            cursor = conn.cursor()
-            #########  init table for database
-            # init_database(conn)
+            c = conn.cursor()
 
-            #########   import from CSV , fastest way
+            ######### init table for database
+            # init_database(c)
+
+            ######### after you fill up the details_of_sensor with, use this to create value table for each site
+            # create_resource_value_tables(c)
+            # create_resource_ETL_tables(c)
+
+            ######### import from CSV demo , fastest way , execute in the sqlite not in the scripts
             # resource_value_folder = "/Users/nanazhu/Documents/Sapienza/sqlite/5min2017Jan-Mar/"
             # importFromCSV(resource_value_folder, "resource_value")
 
+            ######### export to CSV demo
+            query = """select time, value from resource_value where time > '2017-03-21' and time < '2017-03-22';"""
+            exportToCSV(query, 'JustATest.csv')
 
-            #########  export to CSV
-            # query = """select time, value from resource_value where time > '2017-03-21' and time < '2017-03-22';"""
-            # exportToCSV(query, 'JustATest.csv')
-
-
-            #########  demo story :find all the resources which are temperature
-            # input
-            # time range : whole March
-            # output: one dataframe for one site
-            #########
-
-            c = conn.cursor()
+            ######### a full demo story:find all the temperature sensor data
+            #######   input
+            #######   time range : whole March
+            ######### output: pandas dataframe for one site
             site_list = c.execute("select site from details_sensor group by site;")
             site_list = [str(id[0]) for id in site_list]
             for site_id in site_list:
@@ -179,8 +232,11 @@ if __name__ == "__main__":
                 temperature_resource_list = query_temperature_resource(c, site_id)
                 print(temperature_resource_list)
                 df = select_time_range_to_dataframe(c, temperature_resource_list, '2017-03-01', '2017-04-01')
-                break
 
+            ######### convert from previous format into ready-to-import-to-sqlite csv file
+            df = pd.read_csv("API_tempC_Febhourly.csv", delimiter=";", parse_dates=True)
+            print(df)
+            pandas_to_sqlite(df)
 
         except Error as e:
             print("SQL ERROR:", e)
