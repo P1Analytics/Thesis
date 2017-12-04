b6c0088fc7429ffa063e0d897b9347dcf08e56d0
nanazhu <nana.zhu@hotmail.com>
2017-12-04 13:28:03 +0100

    clear the code

--- src_python/Data_Interpretation_SideEffectsOnIndoorTemperature.py
+++ src_python/Data_Interpretation_SideEffectsOnIndoorTemperature.py
@@ -1,95 +1,11 @@
-import time
-import operator
-from collections import Counter
 from Data_Preparation import *
-
 warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 
-def sun_rise_set(lat, lng, timestamp):
-    timezone_url = "https://maps.googleapis.com/maps/api/timezone/json?location=" + str(lat) + "," + str(lng) + \
-                   "&timestamp=" + str(timestamp) + \
-                   "&key=AIzaSyAI4--_x4AE2K5zZ6Z5tZafwwpVI9uYlYM"
-    # print(timezone_url)
-    rs = requests.get(timezone_url).json()
-    # print(json.dumps(rs, sort_keys=True, indent=4))  # human-readable response :)
-    GMT = int((int(rs["dstOffset"]) + int(rs["rawOffset"])) / 3600)
-
-    rise_set_url = "https://api.sunrise-sunset.org/json?lat=" + str(lat) + "&lng=" + str(lng) + \
-                   "&date=" + time.strftime("%D", time.localtime(timestamp))
-    try:
-        rise_set = requests.get(rise_set_url).json()
-    except:
-        return 8, 12, 18
-    sunrise = int(rise_set["results"]["sunrise"].split(":")[0]) + GMT
-    noon = int(rise_set["results"]["solar_noon"].split(":")[0]) + GMT
-    sunset = int(rise_set["results"]["sunset"].split(":")[0]) + 12 + GMT
-    return sunrise, noon, sunset
-
-
-def predict_orientation(df_rooms, active_rooms, date_list):
-    prediction = []
-    peak_dict = defaultdict(list)
-    peak_top3 = defaultdict(list)
-    sunrise, noon, sunset = sun_rise_set(lat, lng, pd.Timestamp(date_list[0] + ' 01:00').timestamp())
-    for date in date_list:
-        if '-15' in date or '-30' in date:  # update the sunrise sunset every 15 days
-            sunrise, noon, sunset = sun_rise_set(lat, lng, pd.Timestamp(date + ' 01:00').timestamp())
-        begin = df_rooms.index.get_loc(date + " " + '{:02d}'.format(sunrise + 2) + ':00:00')
-        end = df_rooms.index.get_loc(date + " " + '{:02d}'.format(sunset) + ':00:00')
-        df = df_rooms.iloc[begin:end]
-        for room in active_rooms:
-            if pd.isnull(df[room]).sum() == len(df.index.values):
-                continue
-            df.index = pd.to_datetime(df.index)
-            peak_top3[room].append(list(map(int, [pd.to_datetime(str(timestamps)).strftime('%H') for timestamps in
-                                                  df[room].nlargest(3).index.values])))
-            peak_dict[room].append(df[room].groupby(pd.TimeGrouper('D')).idxmax().dt.hour.values[0])
-
-    for room in active_rooms:
-        peak = peak_dict[room]
-        sum = 0
-        east = False
-        morning = []
-        morning_counter = 0
-        for hour in peak:
-            sum += hour / 24 * 360
-            if sunrise <= hour <= noon - 1:
-                east = True
-                morning.append(hour)
-                morning_counter += 1
-        peak_in_morning_ratio = morning_counter / len(peak)
-        morning_peak_ratio = 0
-        if east:
-            morning_peak_ratio = peak_in_morning_ratio
-        degree_prediction = sum / len(peak)
-
-        if 0 <= degree_prediction < sunrise / 24 * 360:
-            result = "NE"
-        if 0.5 < peak_in_morning_ratio or sunrise / 24 * 360 <= degree_prediction < noon / 24 * 360:
-            result = "SE"
-        if noon / 24 * 360 <= degree_prediction < sunset / 24 * 360:
-            result = "SW"
-        if sunset / 24 * 360 <= degree_prediction < 359:
-            result = "NW"
-        prediction.append([room, result])
-
-        peak_top3[room] = [item for sublist in peak_top3[room] for item in sublist]
-
-        # print(room, '{:05.1f}'.format(degree_prediction), '{:05.2f}%'.format(morning_peak_ratio * 100),
-        #       "peak hour,frequency:", sorted(dict(Counter(peak)).items(), key=operator.itemgetter(1), reverse=True),
-        #       "temperature range: [",
-        #       '{:05.2f}'.format(np.percentile(df[room].values, 0)),
-        #       '{:05.2f}'.format(np.percentile(df[room].values, 50)),
-        #       '{:05.2f}]'.format(np.percentile(df[room].values, 100)))
-
-    return prediction, peak_top3
-
-
-def retrieve_human_active(database, Year, Months, feq=None):
+def retrieve_period_data(database, Year, Months, feq=None):
     """
     retrieve data from database during date rage A-B
-    :param feq: 5 mins sample use None; if 1hour sample use "00:00" ; 1 day sample use "21:00"
+    :param feq: 5 mins sample use None; 1hour sample use "00:00" ; 1 day sample use "21:00"
     :param database: sqlite database file
     :param Year :
     :param Months: query months range
@@ -112,12 +28,12 @@ def retrieve_human_active(database, Year, Months, feq=None):
             site_list = c.execute(
                 "select site from details_sensor group by site;")  # TODO or fill the table :details_site
             site_list = [str(id[0]) for id in site_list]
-            site_list=[
-                #"144024","28843", "144243", "28850",
-                "144242",#"155849", #"144242",  "19640", "27827", "155849",
-            #     "155851", "155076", "155865", "155077",
-            #     "155877", "157185", "159705"
-            ]
+            # site_list=[
+            #     #"144024","28843", "144243", "28850",
+            #     "144242",#"155849", #"144242",  "19640", "27827", "155849",
+            # #     "155851", "155076", "155865", "155077",
+            # #     "155877", "157185", "159705"
+            # ]
             for site_id in site_list:
                 temperature_resource_list = [i[0] for i in query_site_room_orientaion(c, site_id)]
                 motion_resource_list = []
@@ -154,11 +70,11 @@ def retrieve_human_active(database, Year, Months, feq=None):
 
 if __name__ == "__main__":
     database = '/Users/nanazhu/Documents/Sapienza/Thesis/src_python/test.db'
-    site_list, dict_df_temp, dict_df_motion, dict_df_cloud, dict_df_tempC = retrieve_human_active(database, Year=2017, Months=list(range(9, 10)), feq=None)
+    site_list, dict_df_temp, dict_df_motion, dict_df_cloud, dict_df_tempC = \
+        retrieve_period_data(database, Year=2017,Months=list(range(9, 10)),feq=None)
     orientation_dict = retrieve_orientation(database)
 
     for site_id in site_list:
-        print("********* ", site_id, " *********")
         rooms_orientation = orientation_dict[int(site_id)]
         if rooms_orientation is None:
             print(site_id, " orientation empty")
@@ -167,8 +83,9 @@ if __name__ == "__main__":
         df_original = dict_df_temp[site_id]
         df_temp, room_list, begin = ETL(df_original)
         if -1 == begin:
-            print(site_id, " empty")
+            print(site_id, "data empty")
             continue
+        print(site_id, ".....")
 
         df_motion = dict_df_motion[site_id]
         df_cloud = dict_df_cloud[site_id]
@@ -207,27 +124,27 @@ if __name__ == "__main__":
                 major_ticks = np.arange(0, xmax, 12)  # 5mins * 12 = 1hour
 
             f, axn = plt.subplots(4, 1, sharex=True, squeeze=False)
-            axn[0,0].set_title(day_i + " at " + site_id)
+            axn[0, 0].set_title(day_i + " at " + site_id)
 
-            df_day_i.plot(ax=axn[0,0])
-            axn[0, 0].legend(room_labels, loc=1)
+            df_day_i.plot(ax=axn[0, 0])
+            axn[0, 0].legend(room_labels, loc=2)
             axn[0, 0].set_xticks([])
 
-            df_motion_i.plot(ax=axn[1,0],marker=".")
-            axn[1, 0].legend(room_labels, loc=1)
+            df_motion_i.plot(ax=axn[1, 0], marker=".")
+            axn[1, 0].legend(room_labels, loc=2)
             axn[1, 0].set_xticks([])
 
             df_cloud_i.plot(ax=axn[2, 0], marker=",")
-            axn[2, 0].legend(["cloud"], loc=1)
+            axn[2, 0].legend(["cloud"], loc=2)
             axn[2, 0].set_xticks([])
 
             df_tempC_i.plot(ax=axn[3, 0], marker="o")
-            axn[3, 0].legend(["TempOut"], loc=1)
+            axn[3, 0].legend(["TempOut"], loc=2)
             axn[3, 0].set_xticks([])
 
             plt.xlim(0, xmax)
             plt.xticks(major_ticks, list(range(24)))
-            f.set_size_inches(18.5, 7.5)
+            f.set_size_inches(18.5, 11.5)
             plt.savefig(day_i + "_" + site_id + '.png', dpi=400)
         plt.close('all')
-    # plt.show()
+        # plt.show()
