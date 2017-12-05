423cdf7e7b44148ebaa5c6938c0117a3741fa006
nanazhu <nana.zhu@hotmail.com>
2017-12-05 12:43:56 +0100

    add comments

--- src_python/Data_Interpretation_SideEffectsOnIndoorTemperature.py
+++ src_python/Data_Interpretation_SideEffectsOnIndoorTemperature.py
@@ -35,16 +35,19 @@ def retrieve_period_data(database, Year, Months, feq=None):
             # #     "155877", "157185", "159705"
             # ]
             for site_id in site_list:
+                # retrieve the resource list for each type of sensor
                 temperature_resource_list = [i[0] for i in query_site_room_orientaion(c, site_id)]
                 motion_resource_list = []
                 for i in temperature_resource_list:
-                    subsite_id = c.execute("select subsite from details_sensor where resource= " + str(
-                        i) + " and subsite !=0 ;").fetchall()[0][0]
-                    resource = c.execute("select resource from details_sensor where subsite = " + str(
-                        subsite_id) + " and property = 'Motion';").fetchall()
+                    subsite_id = c.execute("select subsite from details_sensor where resource= " +
+                                           str(i) + " and subsite !=0 ;").fetchall()[0][0]
+                    resource = c.execute("select resource from details_sensor where subsite = " +
+                                         str(subsite_id) + " and property = 'Motion';").fetchall()
                     if resource:
                         resource = resource[0][0]
                         motion_resource_list.append(resource)
+
+                # retrieve the sensor data
                 if feq:
                     dict_df_temp[site_id] = select_time_range_to_dataframe(c, site_id, temperature_resource_list,
                                                                            date_A, date_B, feq)
@@ -55,7 +58,7 @@ def retrieve_period_data(database, Year, Months, feq=None):
                                                                            date_A, date_B)
                     dict_df_motion[site_id] = select_time_range_to_dataframe(c, site_id, motion_resource_list, date_A,
                                                                              date_B)
-
+                # retrieve the API data
                 query = " select time,value from API_CloudCoverage " \
                         "where id=" + site_id + " and (time> '" + date_A + "' and time < '" + date_B + "');"
                 dict_df_cloud[site_id] = select_single_sensor_to_pandas(c, query, site_id)
@@ -69,48 +72,60 @@ def retrieve_period_data(database, Year, Months, feq=None):
 
 
 if __name__ == "__main__":
+
+    # collect the data from database
     database = '/Users/nanazhu/Documents/Sapienza/Thesis/src_python/test.db'
     site_list, dict_df_temp, dict_df_motion, dict_df_cloud, dict_df_tempC = \
         retrieve_period_data(database, Year=2017,Months=list(range(9, 10)),feq=None)
     orientation_dict = retrieve_orientation(database)
 
+    # process each school
     for site_id in site_list:
+        # if we don' know the orientation  , jump to next school
         rooms_orientation = orientation_dict[int(site_id)]
         if rooms_orientation is None:
             print(site_id, " orientation empty")
             continue
 
+        # check the indoor temperature if it is empty , jump to next school
         df_original = dict_df_temp[site_id]
         df_temp, room_list, begin = ETL(df_original)
         if -1 == begin:
-            print(site_id, "data empty")
+            print(site_id, " data empty")
             continue
         print(site_id, ".....")
 
         df_motion = dict_df_motion[site_id]
         df_cloud = dict_df_cloud[site_id]
-        df_cloud = df_cloud.reindex(df_original.index, method='pad', limit=11)
+        df_cloud = df_cloud.reindex(df_original.index, method='pad', limit=11) # expand the hour sample to 5mins sample
 
         df_tempC = dict_df_tempC[site_id]
         df_tempC = df_tempC.reindex(df_original.index, method='pad', limit=11)
 
         day_index = [pd.to_datetime(str(date)).strftime('%Y-%m-%d') for date in df_original.index.values]
+
+        # replacing index for date for plot
         day_list = sorted(set(day_index))
         df_motion.index = day_index
         df_temp.index = day_index
         df_cloud.index = day_index
         df_tempC.index = day_index
 
-        room_labels = []
+        # prepare for the legend of each room
+        room_legends = []
         i = 1
         for room_id in room_list:
             for id_label in rooms_orientation:
                 if room_id == id_label[0]:
-                    room_labels.append("R" + str(i) + "_" + id_label[1] + "_" + str(room_id))
+                    room_legends.append("R" + str(i) + "_" + id_label[1] + "_" + str(room_id))
                     i += 1
                     break
 
-        for day_i in day_list[20:30]:
+
+        # plot out the data per day
+        for day_i in day_list[0:30]:
+            if pd.to_datetime(day_i).dayofweek <5:
+                continue
 
             df_day_i = df_temp.loc[day_i, :]
             df_motion_i = df_motion.loc[day_i]
@@ -127,11 +142,11 @@ if __name__ == "__main__":
             axn[0, 0].set_title(day_i + " at " + site_id)
 
             df_day_i.plot(ax=axn[0, 0])
-            axn[0, 0].legend(room_labels, loc=2)
+            axn[0, 0].legend(room_legends, loc=2)
             axn[0, 0].set_xticks([])
 
             df_motion_i.plot(ax=axn[1, 0], marker=".")
-            axn[1, 0].legend(room_labels, loc=2)
+            axn[1, 0].legend(room_legends, loc=2)
             axn[1, 0].set_xticks([])
 
             df_cloud_i.plot(ax=axn[2, 0], marker=",")
